b0d3636e9ef1206e6954f21a79a3d3464944d103
Mendel E <mendelmaleh@gmail.com>
2019-08-08 16:13:12 -0400

    boilerplate

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('requirements.txt', encoding='utf-8') as r:
+    requires = [i.strip() for i in r]
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='wttr',
+    version='0.4',
+    description='wttr.in wrapper for cli',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mendelmaleh/wttr.py'
+    author='Mendel E.'
+    py_modules=['wttr'],
+    scripts=['wttr', 'wttr.py'],
+    install_requires=requires,
+)
