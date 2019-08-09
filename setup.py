a20a0adcbb01bc600dc2fc820935bdd9e431dea0
Mendel E <mendelmaleh@gmail.com>
2019-08-08 21:48:11 -0400

    bump ver, fix syntax errors

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='wttr',
-    version='0.4',
+    version='0.4.1',
     description='wttr.in wrapper for cli',
     long_description=readme,
     long_description_content_type='text/markdown',
-    url='https://github.com/mendelmaleh/wttr.py'
-    author='Mendel E.'
+    url='https://github.com/mendelmaleh/wttr.py',
+    author='Mendel E.',
     py_modules=['wttr'],
     scripts=['wttr', 'wttr.py'],
     install_requires=requires,
